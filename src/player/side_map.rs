904ae46080293738fb860a912b056f1b7b5175bf
moroviintaas <moroviintaas@gmail.com>
2023-07-23 13:20:14 +0200

    Created some code for key mapping in SideMap

--- src/player/side_map.rs
+++ src/player/side_map.rs
@@ -1,5 +1,4 @@
 use std::ops::{Index, IndexMut};
-use sztorm::SMap;
 use crate::player::side::{Side, SIDES};
 use crate::player::side::Side::{East, North, South, West};
 #[cfg(feature="speedy")]
@@ -119,115 +118,11 @@ impl<T> IndexMut<&Side> for SideMap<T>{
         }
     }
 }
-/*
-impl<T> Index<Side> for SideMap<T>{
-    type Output = T;
 
-    fn index(&self, index: Side) -> &Self::Output {
-        match index{
-            East => &self.east,
-            South => &self.south,
-            West => &self.west,
-            North => &self.north
-        }
-    }
-}
-
-impl<T> IndexMut<Side> for SideMap<T>{
-    fn index_mut(&mut self, index: Side) -> &mut Self::Output {
-        match index{
-            East => &mut self.east,
-            South => &mut self.south,
-            West => &mut self.west,
-            North => &mut self.north
-        }
-    }
-}
-*/
 impl<T: Eq> SideMap<T>{
     pub fn are_all_equal(&self) -> bool{
         let t_north = &self.north;
         self.and(|c| c== t_north)
-    }
-}
-
-pub struct SideMapKeyIter<'a>{
-    keys: [&'a Side; 4],
-    index: usize,
-}
-
-pub struct SideMapIter<'a, V: 'a>{
-    keys: [Side; 4],
-    map: &'a SideMap<V>,
-    index: usize
-}
-
-impl<'a> Default for SideMapKeyIter<'a>{
-    fn default() -> Self {
-        SideMapKeyIter{
-            keys: [&North, &East, &West, &South],
-            index: 0
-        }
-    }
-}
-
-impl<'a, V:'a> SideMapIter<'a, V>{
-    pub fn new(map: &'a SideMap<V>) -> Self{
-        Self{
-            keys: [North, East, West, South],
-            index: 0,
-            map
-        }
-    }
-}
-
-impl<'a> Iterator for SideMapKeyIter<'a>{
-    type Item = &'a Side;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.index{
-            n@ 0..=3 => {
-                self.index +=1;
-                Some(self.keys[n])
-            },
-            _ => None
-        }
-    }
-}
-impl<'a, V: 'a> Iterator for SideMapIter<'a, V>{
-    type Item = (Side, &'a V);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.index{
-            n@ 0..=3 => {
-                self.index +=1;
-                Some((self.keys[n], &self.map[&self.keys[n]]))
-            },
-            _ => None
-        }
-    }
-}
-
-/// ```
-/// use brydz_core::player::side::SideMap;
-/// use sztorm::SMap;
-/// let map = SideMap::new_symmetric(true);
-/// assert!(map.all(|v| *v ))
-/// ```
-impl<'a, V: 'a> SMap<'a, Side, V> for SideMap<V>
-where Self: 'a{
-    type Iter = SideMapIter<'a, V>;
-
-    fn get_value(&self, key: Side) -> Option<&V> {
-        Some(&self[&key])
-    }
-
-    fn get_mut_value(&mut self, key: Side) -> Option<&mut V> {
-        Some(&mut self[&key])
-    }
-
-    fn iter(&'a self) -> Self::Iter {
-        SideMapIter::new(&self)
     }
 }
 
