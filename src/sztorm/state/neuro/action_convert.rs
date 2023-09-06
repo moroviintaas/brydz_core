b92e54cd12734577cac0c160f4e9620afb456361
moroviintaas <moroviintaas@gmail.com>
2023-09-06 20:52:06 +0200

    Implemented conversion from action to Tensor

---
+++ src/sztorm/state/neuro/action_convert.rs
@@ -0,0 +1,27 @@
+use tch::Tensor;
+use karty::cards::Card2SymTrait;
+use karty::symbol::CardSymbol;
+use sztorm_rl::tensor_repr::{ConvertToTensor, WayToTensor};
+use crate::sztorm::state::ContractAction;
+
+pub struct ContractActionWayToTensor{
+
+}
+
+impl WayToTensor for ContractActionWayToTensor{
+    fn desired_shape() -> &'static [i64] {
+        &[2]
+    }
+}
+
+impl ConvertToTensor<ContractActionWayToTensor> for ContractAction{
+    fn to_tensor(&self, way: &ContractActionWayToTensor) -> Tensor {
+        match self{
+            ContractAction::ShowHand(_) => {panic!("Not prepared to convert show hand to tensor")}
+            ContractAction::PlaceCard(c) => {
+                let v = vec![c.suit().usize_index() as f32, c.figure().usize_index() as f32];
+                Tensor::from_slice(&v[..])
+            }
+        }
+    }
+}
--- src/sztorm/state/neuro/mod.rs
+++ src/sztorm/state/neuro/mod.rs
@@ -1,3 +1,11 @@
 mod as_state_history_tensor;
+mod action_convert;
 
+use tch::Tensor;
 pub use as_state_history_tensor::*;
+pub use action_convert::*;
+use karty::cards::Card2SymTrait;
+use karty::symbol::CardSymbol;
+use sztorm_rl::tensor_repr::{ConvertToTensor, WayToTensor};
+use crate::sztorm::state::ContractAction;
+
