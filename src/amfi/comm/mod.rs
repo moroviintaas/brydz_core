354418da9aa4a6604a833654a4ce8a20ad394007
moroviintaas <moroviintaas@gmail.com>
2023-10-27 12:48:03 +0200

    Adjusted to rename of CommError to CommunicationError

--- src/amfi/comm/mod.rs
+++ src/amfi/comm/mod.rs
@@ -1,7 +1,7 @@
 use amfi::comm::SyncComm;
-use amfi::error::CommError;
+use amfi::error::CommunicationError;
 use amfi::domain::{AgentMessage, EnvMessage};
 use crate::amfi::spec::ContractDP;
 
-pub type ContractAgentSyncComm = SyncComm<AgentMessage<ContractDP>, EnvMessage<ContractDP>, CommError<ContractDP>>;
-pub type ContractEnvSyncComm = SyncComm<EnvMessage<ContractDP>, AgentMessage<ContractDP>, CommError<ContractDP>>;
+pub type ContractAgentSyncComm = SyncComm<AgentMessage<ContractDP>, EnvMessage<ContractDP>, CommunicationError<ContractDP>>;
+pub type ContractEnvSyncComm = SyncComm<EnvMessage<ContractDP>, AgentMessage<ContractDP>, CommunicationError<ContractDP>>;
