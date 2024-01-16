8eb5ddd8aa9330636625545dd29153538b29f6e4
moroviintaas <moroviintaas@gmail.com>
2024-01-16 18:30:08 +0100

    Preparing version 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 //! Module containing basic Symbol trait
 //! # Licence:
 //! MIT: [https://mit-license.org/](https://mit-license.org/)
-//! # Authors:
-//! [morovintaas](mailto:moroviintaas@gmail.com)
 //!
+//! This crate implements core rules of contract bridge game (currently only playing phase).
+//! With feature `amfiteatr` it provides interfaces compatible with [`amfiteatr`](https://github.com/moroviintaas/amfiteatr).
 //!
+//! This crate is published in current state to be some live example of `amfiteatr` concept.
+//! Currently it lacks documentation and is not stable.
 pub mod player;
 pub mod contract;
 pub mod score;
